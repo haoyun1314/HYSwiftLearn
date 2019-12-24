2fffc20cc9ce2d1d94fac2a410b0ffd77efbdaa9
范浩云 <13241143266@163.com>
2019-12-24 16:19:22 +0800

    提交

--- HYSwiftLearn/HYSwiftLearn/ViewController.swift
+++ HYSwiftLearn/HYSwiftLearn/ViewController.swift
@@ -8,10 +8,103 @@
 
 import UIKit
 import Foundation
+
+
+
+// 1.定义结构体
+// 在swift中结构体或者类在实例化时,必须保证所有的属性有初始化值.如果没有编译不会通过
+// 如果自定义构造函数,那么默认的构造函数会被覆盖掉,如果不希望被覆盖,必须明确的实现该构造函数
+// 如果给结构体扩充方法,则必须在方法的前面加mutating
+struct Location {
+    var x : Double
+    var y : Double
+    
+    init () {
+        x = 0
+        y = 0
+    }
+    
+    init (x : Double, y : Double) {
+        self.x = x
+        self.y = y
+    }
+    
+    init (xyStr : String) {
+        let xyArray = xyStr.split(separator:",")
+        self.x = Double(xyArray.first!)!
+        self.y = Double(xyArray.last!)!
+    }
+    
+    mutating func moveH(x : Double) {
+        self.x += x
+    }
+    
+    mutating func moveV(y : Double) {
+        self.y += y
+    }
+}
+
+// 2.创建结构体实例
+// let location = Location(x: 100, y: 100)
+//let location = Location()
+//let location1 = Location(x: 100, y: 100)
+//let location2 = Location(xyStr: "100,100")
+
+
+// 3.给系统的结构体扩充方法
+extension CGPoint {
+    mutating func moveH(x : CGFloat) {
+        self.x += x
+    }
+    
+    mutating func moveV(y : CGFloat) {
+        self.y += y
+    }
+}
+
+
+
+
+
+// 1.定义枚举类型
+enum Direction {
+    case East
+    case West
+    case North
+    case South
+}
+
+
+
+// 2.给枚举类型指定值
+enum Sex : Int {
+    case Man = 0
+    case Weman = 1
+}
+
+
+
+//// 1.定义结构体,用于描述位置信息
+//struct Location {
+//    var x : Double
+//    var y : Double
+//}
+
+// 2.定义坐标点
+ let center = Location(x: 100, y: 100)
+ let testPoint = Location(x: 80, y: 70)
+
+
+
+
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         
 //      TODO:Swift初体验
         // 2.定义一个标识符时必须制定该标识符时变量还是常量
@@ -432,12 +525,442 @@ class ViewController: UIViewController {
            }
         
         
+//        TODO:函数
+        self.about();
+        self.about1();
+        let getString =  self.readMessage()
+        print(getString)
+        self.callPhone(phoneNum: "你好么");
+        // (Int, Int) -> Int
+        var mathFunction : (Int, Int) -> Int = sumTwoInts
+        print(mathFunction(20, 10))
+        
+        
+        func multiplyTwoInts(num1 : Int, num2 : Int) -> Int {
+            return num1 * num2
+        }
+        mathFunction = multiplyTwoInts
+        print(mathFunction(40, 10))
+        
+        
+//        / 3.将函数作为方法的返回值
+        var km = -8
+
+        func stepForward(num : Int) -> Int {
+            return num + 1
+        }
+
+        func stepBackward(num : Int) -> Int {
+            return num - 1
+        }
+
+    
+//        var stepFunc : (Int) -> Int
+//        if m < 0 {
+//            stepFunc = stepForward
+//        } else {
+//            stepFunc = stepBackward
+//        }
+//
+        func getStepFunc(num : Int) -> (Int) -> Int {
+            return num < 0 ? stepForward : stepBackward
+        }
+
+        let stepFunc = getStepFunc(num: 100)
+        
+        let currentInt : Int = stepFunc(10)
+        
+        print(currentInt)
+        
+        
+
+            //let count = getNumberCount(nums)
+            //count.0
+            //count.1
+
+            
+        //    TODO:枚举
+            
+                var kd1 = Direction.East
+                kd1 = Direction.West
+                // let d : Direction = .East
+                let ksex = Sex(rawValue: 0)
+        
+        print(kd1);
+        print(ksex ?? 19)
+        
+            
+        
+        print(center)
+        print(testPoint)
+        
+//        inRange(point: testPoint)
+        
+        print(inRange(point: testPoint))
+        
+        
+
+        // 4.系统结构体创建方式补充
+        var point = CGPoint()
+        point.x = 100
+        point.y = 100
+        let size = CGSize(width: 50, height: 50)
+        let rect = CGRect(x: 10, y: 10, width: 100, height: 100)
+        let range = NSRange(location: 5, length: 10)
+        
+        print(size)
+        print(rect)
+        print(range)
         
         
         
-        
-        
+        // 1.创建Student对象
+        let stu = Student()
+        stu.name = "why"
+        stu.age = 10
+        stu.mathScore = 100
+        stu.chineseScore = 90
+        print(stu.mathScore)
+        // 2.计算该学生的平均成绩
+        // print(stu.getAverageScore())
+        print(stu.averageScore)
+        //stu.averageScore = 98
+        print(stu.chineseScore)
+
+        // 3.设置学生的课程数
+        Student.courseCount = 2
+
+
+}
+    
+    
+    // 3.写一个方法用于进行判断
+    func inRange(point : Location) -> Bool {
+        let disX = point.x - center.x
+        let disY = point.y - center.y
+        let dis = sqrt(pow(disX, 2) + pow(disY, 2))
+        return dis < 200
     }
+
+    
+    
+    // 2.将函数作为方法的参数进行传递
+    func printResult(num1 : Int, num2 : Int, mathFunc : (Int, Int) -> Int) {
+        print(mathFunc(num1, num2))
+    }
+    // 1.定义一个函数
+    func sumTwoInts(num1 : Int, num2 : Int) -> Int {
+        return num1 + num2
+    }
+
+    
+    // 1.没有参数没有返回值的函数
+    func about() -> Void {
+        print("iPhone6 Plus")
+    }
+
+    func about1() -> Void {
+        print("iPhone6s Plus")
+    }
+
+    // 2.没有参数有返回值的函数
+    func readMessage() -> String {
+        return "吃饭了吗?"
+    }
+
+    // 3.有参数,没有返回值的函数
+    func callPhone(phoneNum : String) {
+        print("打电话给:\(phoneNum)")
+    }
+
+    // 4.有参数有返回值的函数
+    func sum(num1 : Int, num2 : Int) -> Int {
+        return num1 + num2
+    }
+
+    // 5.有参数有多个返回值的函数
+    let nums = [1, 3, 6, 8, 12, 11, 22, 24]
+
+    func getNumberCount(nums : [Int]) -> (Int, Int) {
+        var oddCount = 0
+        var evenCount = 0
+        
+        for num in nums {
+            if num % 2 == 0 {
+                oddCount+1
+            } else {
+                evenCount+1
+            }
+        }
+        
+        return (oddCount, evenCount)
+    }
+
 
 }
 
+
+
+
+class Student {
+    // 1.存储属性
+    var name : String = ""
+    var age : Int = 0
+    var mathScore : Double = 0
+    var chineseScore : Double = 0
+    
+    // 2.提供了函数,可以获取平均成绩
+    /*
+    func getAverageScore() -> Double {
+        return (mathScore + chineseScore) * 0.5
+    }
+    */
+    // 3.通过计算属性获取平均成绩
+    /*
+     1.如果一个计算属性只有get方法,那么该属性称之为只读属性
+        如果只有get方法,那么get{}可以省略
+     2.如果有写set方法,在set方法中有一个newValue常量,用于保存外界传入的值
+        也可以给newValue起一个新的名字,在set后面跟上(newScore)
+    */
+    /*
+    var averageScore : Double {
+        get {
+            return (mathScore + chineseScore) * 0.5
+        }
+        
+        set (newScore) {
+            chineseScore = newScore * 2 - mathScore
+        }
+    }
+    */
+    
+    var averageScore : Double {
+        return (mathScore + chineseScore) * 0.5
+    }
+    // 4.类属性:和整个类相关的属性,并且是通过类进行访问的
+    static var courseCount : Int = 0
+}
+
+
+
+
+
+/*
+ 1.自定义构造函数时会覆盖原有的构造函数
+    如果不希望覆盖,则可以明确的将原来的构造函数写出
+*/
+
+class Person {
+    var name : String = ""
+    var age : Int = 0
+    
+    init() {
+        
+    }
+    
+    init(name : String, age : Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    init(dict : [String : AnyObject]) {
+        if let name = dict["name"] as? String {
+            self.name = name
+        }
+        
+        if let age = dict["age"] as? Int {
+            self.age = age
+        }
+    }
+    
+    
+    
+  
+}
+
+
+
+
+
+class Person1 : NSObject {
+    var name : String = ""
+    var age : Int = 0
+    
+    init(dict : [String : AnyObject]) {
+        super.init()
+        self.setValuesForKeys(dict)
+    }
+    
+//    override func setValue(value: AnyObject?, forUndefinedKey key: String) {}
+}
+
+
+
+
+
+class Person2 {
+    var name : String = ""
+    var age : Int = 0
+    
+    func readBook() {
+        print("人在读书")
+    }
+}
+
+
+class Student2 : Person2 {
+    var sno : Int = 0
+    
+    // 在swift中如果对父类的方法不满意,可以对父类方法进行重写,但是在重写时,必须前面加override
+    override func readBook() {
+        print("学生在读书学习")
+    }
+}
+
+
+
+
+
+
+// 特性一: 继承
+class kPerson {
+    var name : String = ""
+    var age : Int = 0
+    
+    func study() {
+        print("人在学习")
+    }
+    
+    required init() {
+        
+    }
+    
+    init(name : String, age : Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+class kStudent : kPerson {
+    var sno : Int = 0
+    
+    init(name : String, age : Int, sno : Int) {
+        self.sno = sno
+        super.init(name: name, age: age)
+    }
+    
+    // 方法的重写:在swift中如果对父类的方法不满意,可以对父类的方法进行重写.但是必须在前面加上override
+    override func study() {
+        print("学生在学习")
+    }
+
+    required init() {
+        fatalError("init() has not been implemented")
+    }
+}
+
+/*
+let stu = Student(name: "lmj", age: 19)
+stu.name = "why"
+stu.age = 18
+stu.study()
+*/
+
+
+
+// 2.方法的重载
+// 方法的重载有两层含义: 1.参数的类型不同 2.参数的个数不同
+class MathTool {
+    func sum(num1 : Int, num2 : Int) -> Int {
+        return num1 + num2
+    }
+    
+    func sum(num1 : Int, num2 : Int, num3 : Int) -> Int {
+        return num1 + num2 + num3
+    }
+    
+    func sum(num1 : Double, num2 : Double) -> Double {
+        return num1 + num2
+    }
+}
+
+
+
+
+// 3.多态
+func stutyInNight(p : Person) {
+    
+    
+}
+
+
+
+//
+//
+//// 1.定义协议
+//protocol SportProtocol : CrazySportProtocol {
+//    func playBasketball()
+//    func playFallball()
+//}
+//
+//// 2.遵守协议
+//// Swift中遵守协议:1.如果有继承自父类,则在父类后面跟, 写上协议 2.如果没有继承,直接写 : 写上协议
+//// 注意:在swift中默认遵守一个协议后,协议中的方法都必须实现,如果没有实现则会报错误
+//class Person {
+//    var name : String = ""
+//    var age : Int = 0
+//
+//    func running() {
+//        print("跑步")
+//    }
+//}
+//
+//extension Person : SportProtocol {
+//    func playBasketball() {
+//        print("玩篮球")
+//    }
+//
+//    func playFallball() {
+//        print("玩足球")
+//    }
+//
+//    func jumping() {
+//        print("蹦极")
+//    }
+//}
+//
+//// 3.协议之间的继承
+//protocol CrazySportProtocol {
+//    func jumping()
+//}
+//
+
+
+
+
+
+//
+//
+//protocol BuyTicketDelegate : class {
+//    func buyTicket()
+//}
+//
+//class Person {
+//    weak var delegate : BuyTicketDelegate?
+//    func gotoBeijing() {
+//        delegate?.buyTicket()
+//    }
+//}
+//
+//
+//class YellowCattle : BuyTicketDelegate {
+//    func buyTicket() {
+//        print("黄牛帮你买了一张票")
+//    }
+//}
+//
+//let yellowCattle = YellowCattle()
+//let person = Person()
+//person.delegate = yellowCattle
+
+//person.gotoBeijing()
