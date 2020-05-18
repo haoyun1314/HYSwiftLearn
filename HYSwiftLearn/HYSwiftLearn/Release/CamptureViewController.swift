7cbc2441d761785e086b39ea64689de8c46d9317
fanhaoyun <fandongdong@zuoyegang.com>
2020-05-18 20:51:42 +0800

    fix

--- HYSwiftLearn/HYSwiftLearn/Release/CamptureViewController.swift
+++ HYSwiftLearn/HYSwiftLearn/Release/CamptureViewController.swift
@@ -9,23 +9,94 @@
 import UIKit
 
 class CamptureViewController: UIViewController {
-
+ 
+    var backButton:UIButton!
+    
+    var camptureButton : SwiftCamBtn!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        self.view.backgroundColor = UIColor.yellow
+        self.setU()
     }
     
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+extension CamptureViewController
+{
+    func setU () {
+        
+        //返回btn
+        self.view.backgroundColor = UIColor.yellow
+        let button:UIButton = UIButton.init(type:UIButton.ButtonType.custom)
+        button.frame = CGRect.init(x: 16, y: 36, width: 80, height: 30)
+        button.setTitle("返回", for: UIControl.State.normal)
+        button.backgroundColor = UIColor.black
+        button.layer.cornerRadius = 15
+        button.layer.masksToBounds = true
+        button .addTarget(self, action:#selector(btnClick), for: UIControl.Event.touchUpInside)
+        backButton = button;
+        self.view.addSubview(button)
+        
+        
+        //拍摄按钮
+        
+        camptureButton = SwiftCamBtn.init(type:UIButton.ButtonType.custom)
+        let W : NSInteger = NSInteger((self.view.frame.size.width - 80.0)/2)
+        let H : NSInteger = NSInteger((self.view.frame.size.height - 100))
+        camptureButton.frame = CGRect.init(x:W, y: H, width: 80, height: 80)
+        camptureButton.setTitle("拍摄", for: UIControl.State.normal)
+        camptureButton.backgroundColor = UIColor.lightGray
+        camptureButton.layer.cornerRadius = 40
+        camptureButton.layer.masksToBounds = true
+        camptureButton.buttonEnabled = true;
+        camptureButton.delegate = self
+        camptureButton .addTarget(self, action:#selector(camptureBtnClick), for: UIControl.Event.touchUpInside)
+        self.view.addSubview(camptureButton)
+        
+        
+        
     }
-    */
+}
 
+extension CamptureViewController : CamButtonDelegate
+{
+    
+    @objc func btnClick() {
+         self.dismiss(animated:true) {
+             print("返回")
+         }
+     }
+    
+    @objc func camptureBtnClick() {
+         self.dismiss(animated:true) {
+             print("返回")
+         }
+     }
+    
+    
+    func buttonWasTapped() {
+        print("buttonWasTapped")
+    }
+    
+    func buttonDidBeginLongPress() {
+        print("buttonDidBeginLongPress")
+        
+    }
+    
+    func buttonDidEndLongPress() {
+        print("buttonDidEndLongPress")
+        
+    }
+    
+    func longPressDidReachMaximumDuration() {
+        print("longPressDidReachMaximumDuration")
+        
+    }
+    
+    func setMaxiumVideoDuration() -> Double {
+        return 10;
+    }
+     
 }
---
+++ HYSwiftLearn/HYSwiftLearn/Release/CamtrueToos/CamButton.swift
@@ -0,0 +1,106 @@
+//
+//  CamButton.swift
+//  HYSwiftLearn
+//
+//  Created by fanhaoyun on 2020/5/18.
+//  Copyright © 2020 范浩云. All rights reserved.
+//
+
+import UIKit
+
+public protocol CamButtonDelegate : class
+{
+         
+     func buttonWasTapped()
+     
+     
+     func buttonDidBeginLongPress()
+     
+
+     func buttonDidEndLongPress()
+     
+     
+     func longPressDidReachMaximumDuration()
+     
+     
+     func setMaxiumVideoDuration() -> Double
+}
+
+open class CamButton: UIButton {
+
+    public weak var delegate: CamButtonDelegate?
+    
+    public var buttonEnabled = true
+    
+    fileprivate var timer : Timer?
+
+    public override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+         createGestureRecognizers()
+    }
+    
+
+    required public init?(coder aDecoder : NSCoder) {
+        super.init(coder: aDecoder)
+        createGestureRecognizers()
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    @objc fileprivate func Tap() {
+        guard buttonEnabled == true else {
+            return
+        }
+       delegate?.buttonWasTapped()
+    }
+    
+    @objc fileprivate func LongPress(_ sender:UILongPressGestureRecognizer!)  {
+          guard buttonEnabled == true else {
+              return
+          }
+          
+          switch sender.state {
+          case .began:
+              delegate?.buttonDidBeginLongPress()
+              startTimer()
+          case .cancelled, .ended, .failed:
+                invalidateTimer()
+              delegate?.buttonDidEndLongPress()
+          default:
+              break
+          }
+      }
+    
+    
+    @objc fileprivate func timerFinished() {
+        invalidateTimer()
+        delegate?.longPressDidReachMaximumDuration()
+    }
+    
+    
+    fileprivate func startTimer() {
+          if let duration = delegate?.setMaxiumVideoDuration() {
+              //Check if duration is set, and greater than zero
+              if duration != 0.0 && duration > 0.0 {
+                  timer = Timer.scheduledTimer(timeInterval: duration, target: self, selector:  #selector(CamButton.timerFinished), userInfo: nil, repeats: false)
+              }
+          }
+      }
+      
+      // End timer if UILongPressGestureRecognizer is ended before time has ended
+      
+      fileprivate func invalidateTimer() {
+          timer?.invalidate()
+          timer = nil
+      }
+    
+    
+    fileprivate func createGestureRecognizers() {
+           let tapGesture = UITapGestureRecognizer(target: self, action: #selector(CamButton.Tap))
+           let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(CamButton.LongPress))
+           self.addGestureRecognizer(tapGesture)
+           self.addGestureRecognizer(longGesture)
+       }
+
+}
